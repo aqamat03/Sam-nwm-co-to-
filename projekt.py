ff6ac50a1a5e31b9fad144d2af1d3b1b6b59397f
aqamat03 <88345090+aqamat03@users.noreply.github.com>
2021-10-16 14:31:12 +0200

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -4,6 +4,7 @@ print("FIRMA KURIERSKA AQAPACK")
 
 zmienna1 = "Witaj"
 zmienna2 = input("Podaj login: ")
+zmienna9 = input("Podaj hasło: ")
 
 print(zmienna1 + " " + zmienna2)
 
@@ -14,5 +15,6 @@ zmienna4 = input("Podaj adres: ")
 zmienna5 = input("Podaj miejscowość: ")
 zmienna6 = input("Podaj kod pocztowy: ")
 zmienna7 = input("Podaj kod kuriera: ")
+zmienna8 = input("Podaj kod paczki: ")
 
-print(zmienna3 + " Adres: " + zmienna4 + " Miejscowość: " + zmienna5 + " Kod pocztowy: " +zmienna6 + " Kod kuriera " + zmienna7)
+print(zmienna3 + " Adres: " + zmienna4 + " Miejscowość: " + zmienna5 + " Kod pocztowy: " +zmienna6 + " Kod kuriera " + zmienna7 + " Kod paczki " + zmienna8)
